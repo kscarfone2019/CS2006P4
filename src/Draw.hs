af8dd18ca802d9200af486968c1354dd49117726
ks233 <ks233@pc2-078-l.cs.st-andrews.ac.uk>
2017-03-29 15:45:02 +0100

    working on draw function

--- src/Draw.hs
+++ src/Draw.hs
@@ -9,5 +9,11 @@ import Board
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
 drawWorld :: World -> Picture
-drawWorld w = Color blue $ Circle 100
+--drawWorld w = Color blue $ Circle 50
 
+ --drawWorld ww = Color blue $ Circle 150
+
+drawWorld w = Pictures [
+                Color white (Circle 5)
+                --Color yellow (Translate (-250) 0 (Circle))
+                ]
