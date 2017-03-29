b47ecd2cb35693551efdda3ee8b03d85c86e9e70
ea50 <ea50@pc2-104-l.cs.st-andrews.ac.uk>
2017-03-29 17:29:45 +0100

    it prints the board, but working of printing the pieces

--- src/Draw.hs
+++ src/Draw.hs
@@ -2,6 +2,7 @@ module Draw(drawWorld) where
 
 import Graphics.Gloss
 import Board
+import Data.Tuple.Select
 
 -- Given a world state, return a Picture which will render the world state.
 -- Currently just draws a single blue circle as a placeholder.
@@ -9,11 +10,64 @@ import Board
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
 drawWorld :: World -> Picture
---drawWorld w = Color blue $ Circle 50
 
- --drawWorld ww = Color blue $ Circle 150
+--Color black (Translate 50 (-250) (circleSolid 40)),
+--Color white (Translate 50 (-250) (circleSolid 40)),
 
 drawWorld w = Pictures [
-                Color white (Circle 5)
-                --Color yellow (Translate (-250) 0 (Circle))
+		--column 1
+                Color yellow (Translate (-250) 150 (rectangleSolid 75 75)),
+                Color yellow (Translate (-250) 50 (rectangleSolid 75 75)),
+                Color yellow (Translate (-250) (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate (-250) 250 (rectangleSolid 75 75)),
+                Color yellow (Translate (-250) (-150) (rectangleSolid 75 75)),
+                Color yellow (Translate (-250) (-250) (rectangleSolid 75 75)),
+
+		--column 2
+		Color yellow (Translate (-150) 150 (rectangleSolid 75 75)),
+		Color yellow (Translate (-150) 50 (rectangleSolid 75 75)),
+		Color yellow (Translate (-150) (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate (-150) 250 (rectangleSolid 75 75)),
+		Color yellow (Translate (-150) (-150) (rectangleSolid 75 75)),
+		Color yellow (Translate (-150) (-250) (rectangleSolid 75 75)),
+
+		--column 3
+		Color yellow (Translate (-50) 150 (rectangleSolid 75 75)),
+		Color yellow (Translate (-50) 50 (rectangleSolid 75 75)),
+		Color yellow (Translate (-50) (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate (-50) 250 (rectangleSolid 75 75)),
+		Color yellow (Translate (-50) (-150) (rectangleSolid 75 75)),
+		Color yellow (Translate (-50) (-250) (rectangleSolid 75 75)),
+
+		--column 4
+		Color yellow (Translate 50 150 (rectangleSolid 75 75)),
+		Color yellow (Translate 50 50 (rectangleSolid 75 75)),
+		Color yellow (Translate 50 (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate 50 250 (rectangleSolid 75 75)),
+		Color yellow (Translate 50 (-150) (rectangleSolid 75 75)),
+		Color yellow (Translate 50 (-250) (rectangleSolid 75 75)),
+
+		--column 5
+		Color yellow (Translate 150 150 (rectangleSolid 75 75)),
+		Color yellow (Translate 150 50 (rectangleSolid 75 75)),
+		Color yellow (Translate 150 (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate 150 250 (rectangleSolid 75 75)),
+		Color yellow (Translate 150 (-150) (rectangleSolid 75 75)),
+		Color yellow (Translate 150 (-250) (rectangleSolid 75 75)),
+
+		--column 6
+		Color yellow (Translate 250 150 (rectangleSolid 75 75)),
+		Color yellow (Translate 250 50 (rectangleSolid 75 75)),
+		Color yellow (Translate 250 (-50) (rectangleSolid 75 75)),
+		Color yellow (Translate 250 250 (rectangleSolid 75 75)),
+		Color yellow (Translate 250 (-150) (rectangleSolid 75 75)),
+		Color yellow (Translate 250 (-250) (rectangleSolid 75 75))
                 ]
+
+pieceHere :: Position -> Board -> Bool
+
+pieceHere pos board = elem pos (map (\ posi -> sel1 board) pieces board)
+
+--getColour :: Position -> Board -> Col
+--getColor = undefined
+
